93a1c271af5d6a932e0d520b663cfcb8f8770479
pooja chavan <pooja.chavan2@sapient.com>
2019-04-11 15:44:21 +0530

    cli1

---
+++ src/ServerStatus.vue
@@ -0,0 +1,25 @@
+<template>
+<div>
+ <h1>Status {{status}}</h1>
+ <button @click ="changeStatus">Change</button>
+</div>
+</template>
+
+<script>
+export default {
+  data: function () {
+    return {
+      status: 'Critical'
+    }
+  },
+  methods: {
+      changeStatus() {
+          this.status = "Changed"
+      }
+  }
+}
+</script>
+
+<style>
+
+</style>
